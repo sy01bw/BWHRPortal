380932a41831cbb1631b1eba60bc13e2b7a69e5c
root <root@6b8a8f8f3348>
2017-04-18 17:43:32 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "BWHRPortal",
+  "homePage" : "Main",
+  "name" : "BWHRPortal",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
